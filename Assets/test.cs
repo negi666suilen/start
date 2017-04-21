5c3c7407827268e5466e10007852891fe1105e20
negi666suilen <0sr4d05d325550h@ezweb.ne.jp>
2017-04-21 16:46:59 +0900

    unity

---
+++ Assets/test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    Transform cube;
+
+
+	// Use this for initialization
+	void Start () {
+        cube = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //上に移動
+        cube.position += new Vector3(0, 0.01f, 0);
+
+		
+	}
+}
